118ac165baeb340a7d24a622f7326b84ea7908ab
poissonfish <niche@vt.edu>
2023-09-15 10:23:40 -0400

    #meeting python scripts

---
+++ dataset.py
@@ -0,0 +1,21 @@
+from torch.utils.data import Dataset
+import torch
+
+
+class CustomDataset(Dataset):
+    def __init__(self, data, transform=None):
+        self.data = data
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        image = item["image"]
+        label = float(item["label"])  # 标签是连续值
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, torch.tensor(label, dtype=torch.float32)
